bcb37b15c53d12d171d3da7ba5f45f6ff74f3003
PolarstarCoffee <98419830+PolarstarCoffee@users.noreply.github.com>
2023-05-22 17:00:11 -0500

    5/22/23
    DONE (For now)
    Missed:
    Interactable objects teehee

--- 181Area/Assets/Scripts/enemy/materialChange.cs
+++ 181Area/Assets/Scripts/enemy/materialChange.cs
@@ -8,16 +8,19 @@ public class materialChange : MonoBehaviour
 
     public Material materialNew; //new material to replace original material ref
     public GameObject enemy; //gameobject to change material for
+    public GameObject enemy2;
 
     private void Start()
     {
-        materialNew = enemy.GetComponent<Renderer>().material; //grab ref to material component on gameObject
+       // materialNew = enemy.GetComponent<Renderer>().material; //grab ref to material component on gameObject
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            Debug.Log("Player Entered");
             enemy.GetComponent<Renderer>().material = materialNew;
+            enemy2.gameObject.GetComponent<Renderer>().material = materialNew;
         }
     }
 }
